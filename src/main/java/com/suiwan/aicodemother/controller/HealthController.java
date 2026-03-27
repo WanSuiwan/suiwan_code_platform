51d97ccf6300aac5371b4955d6e0581f064a3c33
WanSuiwan <Cxp241027241@163.com>
2026-03-27 20:59:58 +0800

    修正错误

---
+++ src/main/java/com/suiwan/aicodemother/controller/HealthController.java
@@ -0,0 +1,18 @@
+package com.suiwan.aicodemother.controller;
+
+import com.suiwan.aicodemother.common.BaseResponse;
+import com.suiwan.aicodemother.common.ResultUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+
+    @GetMapping("/")
+    public BaseResponse<String> healthCheck() {
+        return ResultUtils.success( "ok");
+    }
+}
+
