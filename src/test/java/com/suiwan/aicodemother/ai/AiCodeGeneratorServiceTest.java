aa35d5a7bc44caf76cbdcaf9d625e45c4bda6c6e
WanSuiwan <Cxp241027241@163.com>
2026-03-27 17:50:08 +0800

    1.提供流式输出，模型解析，文件存储功能 2.使用了设计模式进行可维护开发

---
+++ src/test/java/com/suiwan/aicodemother/ai/AiCodeGeneratorServiceTest.java
@@ -0,0 +1,30 @@
+package com.suiwan.aicodemother.ai;
+
+import com.suiwan.aicodemother.ai.model.HtmlCodeResult;
+import com.suiwan.aicodemother.ai.model.MultiFileCodeResult;
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class AiCodeGeneratorServiceTest {
+
+    @Resource
+    private AiCodeGeneratorService aiCodeGeneratorService;
+
+    @Test
+    void generateHtmlCode() {
+        HtmlCodeResult result = aiCodeGeneratorService.generateHtmlCode("做个程序员鱼皮的工作记录小工具");
+        Assertions.assertNotNull(result);
+    }
+
+    @Test
+    void generateMultiFileCode() {
+        MultiFileCodeResult multiFileCode = aiCodeGeneratorService.generateMultiFileCode("做个程序员鱼皮的留言板");
+        Assertions.assertNotNull(multiFileCode);
+    }
+
+}
+
