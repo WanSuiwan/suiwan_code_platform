cbf0f3023df7fec4201ae6db01c4990ff00437e5
WanSuiwan <Cxp241027241@163.com>
2026-03-28 15:24:14 +0800

    完善项目功能，使用nginx进行前端页面部署以及基本的静态显示功能。

--- src/main/java/com/suiwan/aicodemother/core/saver/CodeFileSaverTemplate.java
+++ src/main/java/com/suiwan/aicodemother/core/saver/CodeFileSaverTemplate.java
@@ -3,6 +3,7 @@ package com.suiwan.aicodemother.core.saver;
 import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.util.IdUtil;
 import cn.hutool.core.util.StrUtil;
+import com.suiwan.aicodemother.constant.AppConstant;
 import com.suiwan.aicodemother.exception.BusinessException;
 import com.suiwan.aicodemother.exception.ErrorCode;
 import com.suiwan.aicodemother.model.enums.CodeGenTypeEnum;
@@ -17,8 +18,9 @@ import java.nio.charset.StandardCharsets;
  */
 public abstract class CodeFileSaverTemplate<T> {
 
+
     // 文件保存根目录
-    protected static final String FILE_SAVE_ROOT_DIR = System.getProperty("user.dir") + "/tmp/code_output";
+    protected static final String FILE_SAVE_ROOT_DIR = AppConstant.CODE_OUTPUT_ROOT_DIR;
 
     /**
      * 模板方法：保存代码的标准流程
@@ -26,11 +28,18 @@ public abstract class CodeFileSaverTemplate<T> {
      * @param result 代码结果对象
      * @return 保存的目录
      */
-    public final File saveCode(T result) {
+    /**
+     * 模板方法：保存代码的标准流程（使用 appId）
+     *
+     * @param result 代码结果对象
+     * @param appId  应用 ID
+     * @return 保存的目录
+     */
+    public final File saveCode(T result, Long appId) {
         // 1. 验证输入
         validateInput(result);
-        // 2. 构建唯一目录
-        String baseDirPath = buildUniqueDir();
+        // 2. 构建基于 appId 的目录
+        String baseDirPath = buildUniqueDir(appId);
         // 3. 保存文件（具体实现由子类提供）
         saveFiles(result, baseDirPath);
         // 4. 返回目录文件对象
@@ -49,17 +58,23 @@ public abstract class CodeFileSaverTemplate<T> {
     }
 
     /**
-     * 构建唯一目录路径
+     * 构建基于 appId 的目录路径
      *
+     * @param appId 应用 ID
      * @return 目录路径
      */
-    protected final String buildUniqueDir() {
+    protected final String buildUniqueDir(Long appId) {
+        if (appId == null) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "应用 ID 不能为空");
+        }
         String codeType = getCodeType().getValue();
-        String uniqueDirName = StrUtil.format("{}_{}", codeType, IdUtil.getSnowflakeNextIdStr());
+        String uniqueDirName = StrUtil.format("{}_{}", codeType, appId);
         String dirPath = FILE_SAVE_ROOT_DIR + File.separator + uniqueDirName;
         FileUtil.mkdir(dirPath);
         return dirPath;
     }
+
+
 
     /**
      * 写入单个文件的工具方法
